2dbe4d1daf6fcdca730b5f682e6edc92956ac13b
Kyoro_p <longcorridor@outlook.jp>
2015-06-27 19:00:47 +0900

    RadioProgramクラスを作成

---
+++ Rosk/Rosk/RadioProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosk
+{
+    class RadioProgram
+    {
+        private string title;
+        private DateTime startDateTime;
+        private uint length;
+        private string information;
+
+        public RadioProgram(string title, DateTime startDateTime, uint length, string information)
+        {
+            this.title = title;
+            this.startDateTime = startDateTime;
+            this.length = length;
+            this.information = information;
+        }
+    }
+}
